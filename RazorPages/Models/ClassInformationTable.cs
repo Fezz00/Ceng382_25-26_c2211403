e7239cfd22015cf5217624afa9797a22151cc604
İlker Feza Çoban <ifcoban@gmail.com>
2025-04-15 12:23:52 +0300

    lab push

--- RazorPages/Models/ClassInformationTable.cs
+++ RazorPages/Models/ClassInformationTable.cs
@@ -2,7 +2,7 @@ namespace RazorPages.Models
 {
     public class ClassInformationTable
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string? ClassName { get; set; }
         public int? StudentCount { get; set; }
         public string? Description { get; set; }
--- RazorPages/obj/Debug/net9.0/RazorPages.AssemblyInfo.cs
+++ RazorPages/obj/Debug/net9.0/RazorPages.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPages")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d79dab38b772aadad79518d53bf9742742226b9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6a0ffaab28fb4a1b0ab1e80d67a190f896d37d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPages")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPages")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
